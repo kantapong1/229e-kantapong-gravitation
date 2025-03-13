2bbadfa7c4224ea7e5a3a055b59760370d88fe6e
kantapong1 <kantapong.thag@bumail.net>
2025-03-13 10:49:25 +0700

    Lab8

---
+++ Assets/Script/Gravity.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    Rigidbody rb;
+    const float G = 0.006674f;
+    public static List<Gravity> gravityObjectList;
+
+    //orbit
+    [SerializeField] bool planets = false;
+    [SerializeField] int orbetSpeed = 1000;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if (gravityObjectList == null)
+        {
+            gravityObjectList = new List<Gravity>();
+        }
+
+        gravityObjectList.Add(this);
+
+        //orbit
+        if (!planets) 
+        {
+            rb.AddForce(Vector3.left * orbetSpeed);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var obj in gravityObjectList)
+        {
+            if (obj != this)
+            {
+                Attract(obj);
+            }
+        }
+
+        //Attract
+    }
+
+    void Attract(Gravity other)
+    {
+        Rigidbody otherRb = other.rb;
+
+        Vector3 direction = rb.position - otherRb.position;
+        float distance = direction.magnitude;
+        float forceMagnitude = G * ((rb.mass * otherRb.mass) / Mathf.Pow(distance, 2));
+        Vector3 finalForce = forceMagnitude * direction.normalized;
+
+        //addforce
+        otherRb.AddForce(finalForce);
+
+
+    }
+}
